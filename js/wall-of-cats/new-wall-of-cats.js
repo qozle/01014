28829bacccd0d28508b0fd4856debe2fd398414b
mflanagan@mpihq.com <mflanagan@mpihq.com>
2022-07-14 17:12:38 -0400

    pushing from remote location

--- js/wall-of-cats/new-wall-of-cats.js
+++ js/wall-of-cats/new-wall-of-cats.js
@@ -1,5 +1,5 @@
 // const ws = new WebSocket("ws://10.6.150.160:1337");
-const ws = new WebSocket("ws://192.168.3.202:1337");
+const ws = new WebSocket("wss://01014.org:1337");
 
 ws.addEventListener("open", () => {
 	console.log("We are connected");
